15e6cad20a1e7e9cf4ccf256fc6866b8faff6944
SakieWayendra <dzakywajendr42727@gmail.com>
2026-06-20 21:20:04 +0700

    Yah coba

--- Tugas Akhir PBO/Models/TugasAkhirPboContext.cs
+++ Tugas Akhir PBO/Models/TugasAkhirPboContext.cs
@@ -37,7 +37,7 @@ public partial class TugasAkhirPboContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=tugas_akhir_pbo;Username=postgres;Password=basda123;");
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=tugas_akhir_pbo;Username=postgres;Password=Dwarfpan271921;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
